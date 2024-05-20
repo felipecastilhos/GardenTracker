a0c3b6298cfcdf6043dd61f8f9ac769ed3c78df7
Felipe Castilhos <felipecastilhos@gmail.com>
2024-05-20 10:05:15 -0300

    feat: set hilt application

---
+++ app/src/main/java/com/felipecastilhos/gardentracker/core/dependencyinjection/AppModule.kt
@@ -0,0 +1,14 @@
+package com.felipecastilhos.gardentracker.core.dependencyinjection
+
+import com.felipecastilhos.gardentracker.core.coroutines.DefaultCoroutineContextProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+    @Provides
+    fun provideDispatcherProvider(): DefaultCoroutineContextProvider = DefaultCoroutineContextProvider()
+}
