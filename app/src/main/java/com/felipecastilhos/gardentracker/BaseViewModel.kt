372b5bb5efd00cdd5dcca4353fa5317438569182
Felipe Castilhos <felipecastilhos@gmail.com>
2024-05-20 10:07:17 -0300

    feat: add BaseViewModel

--- app/src/main/java/com/felipecastilhos/gardentracker/BaseViewModel.kt
+++ app/src/main/java/com/felipecastilhos/gardentracker/BaseViewModel.kt
@@ -1,5 +1,25 @@
 package com.felipecastilhos.gardentracker
 
-class BaseViewModel {
+import androidx.lifecycle.ViewModel
+import com.felipecastilhos.gardentracker.core.coroutines.CoroutineContextProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
+open class BaseViewModel(
+    private val dispatcherProvider: CoroutineContextProvider,
+): ViewModel(), CoroutineScope {
+    override val coroutineContext: CoroutineContext = dispatcherProvider.main
+
+    protected fun launchOnIO(block: suspend CoroutineScope.() -> Unit) {
+        launch(dispatcherProvider.io) {
+            block()
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineContext.cancel()
+    }
 }
