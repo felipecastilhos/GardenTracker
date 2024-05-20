eb2473f1cbf6b12559ea90e4bb0aee677d113888
Felipe Castilhos <felipecastilhos@gmail.com>
2024-05-20 10:06:01 -0300

    feat: add CoroutineDispatcher

---
+++ app/src/main/java/com/felipecastilhos/gardentracker/core/coroutines/CoroutineContextProvider.kt
@@ -0,0 +1,43 @@
+package com.felipecastilhos.gardentracker.core.coroutines
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Class to abstract the direct usage of dispatcher
+ * This helps to use inject coroutines in tests
+ */
+interface CoroutineContextProvider {
+    /**
+    +-----------------------------------+
+    | Main thread on Android, interact  |
+    | with the UI and perform light     |
+    | work                              |
+    +-----------------------------------+
+    | - Calling suspend functions       |
+    | - Call UI functions               |
+    | - Updating LiveData               |
+    +-----------------------------------+
+     */
+    val main: CoroutineDispatcher
+
+    /**
+    +-----------------------------------+
+    | Optimized for disk and network IO |
+    | off the main thread               |
+    +-----------------------------------+
+    | - Database                        |
+    | - Reading/writing files           |
+    | - Networking                      |
+    +-----------------------------------+
+     */
+    val io: CoroutineDispatcher
+}
+
+@Singleton
+class DefaultCoroutineContextProvider @Inject constructor() : CoroutineContextProvider {
+    override val main: CoroutineDispatcher = Dispatchers.Main
+    override val io: CoroutineDispatcher =  Dispatchers.IO
+}
