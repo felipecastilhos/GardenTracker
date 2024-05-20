6dc62b9944360ff3f6d80725f769b9931aa8cfa6
Felipe Castilhos <felipecastilhos@gmail.com>
2024-05-20 10:05:15 -0300

    feat: set hilt application

---
+++ app/src/main/java/com/felipecastilhos/gardentracker/GardenTrackerApplication.kt
@@ -0,0 +1,7 @@
+package com.felipecastilhos.gardentracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GardenTrackerApplication: Application()
